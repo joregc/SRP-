56e4f804cfc7087cb7115836133ee67dd3dfc787
jorge-gomez5 <jorge.gomez5@hp.com>
2018-02-19 15:15:55 -0600

    first commit

---
+++ SRP/AccountantEmployee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+  class AccountantEmployee : Employee
+  {
+
+    public void disposedTrash() {
+      Console.WriteLine("disposing trash...");
+    }
+
+    public void ConnectToDB() {
+      Console.WriteLine("connecting to DB...");
+    }
+
+    public void CalculateTaxes() {
+      Console.WriteLine("Calculating taxes...");
+    }
+  }
+}
---
+++ SRP/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+  class Employee
+  {
+
+    public void sendToPrinter() {
+      Console.WriteLine("printing...");
+    }
+
+    public void 
+  }
+}
